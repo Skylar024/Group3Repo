32cf998b58d4ddc9308316e73217b7ab399c146f
joswalt9 <156866636+joswalt9@users.noreply.github.com>
2024-11-12 14:09:46 -0500

    Update Placeholder.js
    Changed Home to Placeholder

--- screens/Placeholder.js
+++ screens/Placeholder.js
@@ -2,7 +2,7 @@ import React from "react";
 import { View, Text } from "react-native";
 import styles from "../styles";
 
-export default function Home() {
+export default function Placeholder() {
   return (
     <View style={styles.container}>
       <Text>Placeholder Content</Text>
