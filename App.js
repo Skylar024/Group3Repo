a8f78b1424bd31b0246cc9a15103a5f83ce086e0
Daniel Renaldo G Santana <149911858+Sandarg95@users.noreply.github.com>
2024-12-09 02:23:40 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Platform } from "react-native";
-import Home from "./screens/Home";
-import Watchlist from "./screens/Watchlist.js";
-import Favorites from "./screens/Favorites.js";
-import TopTen from "./screens/TopTen.js";
-import MovieDetail from "./screens/MovieDetail.js";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import Home from "./screens/Home";
+import Watchlist from "./screens/Watchlist";
+import Favorites from "./screens/Favorites";
+import TopTen from "./screens/TopTen";
+import Search from "./screens/Search";
+import MovieDetail from "./screens/MovieDetail";
 
 const Tab = createBottomTabNavigator();
 const Drawer = createDrawerNavigator();
@@ -30,67 +33,83 @@ function HomeStack() {
   );
 }
 
-// Screen options for Tab and Drawer
 const screenOptions = {
   headerStyle: {
-    backgroundColor: "#14161a", // Top of page(Header)
+    backgroundColor: "#14161a", // Header background color
   },
-  headerTintColor: "#767f88", // Header Text Color
+  headerTintColor: "#767f88", // Header text color
   tabBarStyle: {
-    backgroundColor: "#14161a", // Bottom of page(Tabs)
+    backgroundColor: "#14161a", // Bottom tab background color
   },
   tabBarActiveTintColor: "#1cc859",
   tabBarInactiveTintColor: "#767f88",
+  drawerStyle: {
+    backgroundColor: "#14161a", // Drawer background color
+  },
+  drawerActiveTintColor: "#1cc859",
+  drawerInactiveTintColor: "#767f88",
 };
 
 export default function App() {
   return (
     <NavigationContainer>
       {Platform.OS === "ios" ? (
-        <Tab.Navigator>
-          <Tab.Screen
-            name="Home"
-            component={HomeStack}
-            options={screenOptions}
-          />
-          <Tab.Screen
-            name="TopTen"
-            component={TopTen}
-            options={screenOptions}
-          />
-          <Tab.Screen
-            name="Watchlist"
-            component={Watchlist}
-            options={screenOptions}
-          />
-          <Tab.Screen
-            name="Favorites"
-            component={Favorites}
-            options={screenOptions}
-          />
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            ...screenOptions,
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+
+              if (route.name === "Home") {
+                iconName = focused ? "home" : "home-outline";
+              } else if (route.name === "Search") {
+                iconName = focused ? "search" : "search-outline";
+              } else if (route.name === "TopTen") {
+                iconName = focused ? "star" : "star-outline";
+              } else if (route.name === "Watchlist") {
+                iconName = focused ? "bookmark" : "bookmark-outline";
+              } else if (route.name === "Favorites") {
+                iconName = focused ? "heart" : "heart-outline";
+              }
+
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+          })}
+        >
+          <Tab.Screen name="Home" component={HomeStack} />
+          <Tab.Screen name="Search" component={Search} />
+          <Tab.Screen name="TopTen" component={TopTen} />
+          <Tab.Screen name="Watchlist" component={Watchlist} />
+          <Tab.Screen name="Favorites" component={Favorites} />
         </Tab.Navigator>
       ) : (
-        <Drawer.Navigator>
-          <Drawer.Screen
-            name="Home"
-            component={HomeStack}
-            options={screenOptions}
-          />
-          <Drawer.Screen
-            name="Top Ten"
-            component={TopTen}
-            options={screenOptions}
-          />
-          <Drawer.Screen
-            name="Watchlist"
-            component={Watchlist}
-            options={screenOptions}
-          />
-          <Drawer.Screen
-            name="Favorites"
-            component={Favorites}
-            options={screenOptions}
-          />
+        <Drawer.Navigator
+          screenOptions={({ route }) => ({
+            ...screenOptions,
+            drawerIcon: ({ focused, color, size }) => {
+              let iconName;
+
+              if (route.name === "Home") {
+                iconName = focused ? "home" : "home-outline";
+              } else if (route.name === "Search") {
+                iconName = focused ? "search" : "search-outline";
+              } else if (route.name === "Top Ten") {
+                iconName = focused ? "star" : "star-outline";
+              } else if (route.name === "Watchlist") {
+                iconName = focused ? "bookmark" : "bookmark-outline";
+              } else if (route.name === "Favorites") {
+                iconName = focused ? "heart" : "heart-outline";
+              }
+
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+          })}
+        >
+          <Drawer.Screen name="Home" component={HomeStack} />
+          <Drawer.Screen name="Search" component={Search} />
+          <Drawer.Screen name="Top Ten" component={TopTen} />
+          <Drawer.Screen name="Watchlist" component={Watchlist} />
+          <Drawer.Screen name="Favorites" component={Favorites} />
         </Drawer.Navigator>
       )}
     </NavigationContainer>
